fed643463a156c7574113c87c7ec4d50a875ec4d
Stefanie Chin <chinsk0303@student.laccd.edu>
2018-08-15 11:34:20 -0700

    feat & style: add Link to homepage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,13 +1,37 @@
 import React, { Component, Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import SearchField from './SearchField'
 
 function SearchPage(){
 	return (
 		<Fragment>
-			<SearchField />
-			<p>Book results</p>
+			<div className='search-books-bar'			>
+				<Link
+					to='/'
+					className='close-search'>
+				</Link>
+				<SearchField
+					className='search-books-input-wrapper'
+				/>
+			</div>
+			<div className='search-books-results'>
+				<p className='books-grid'>Book</p>
+			</div>
 		</Fragment>
 	)
 }
 
 export default SearchPage
+
+        //PAGE   <div className="search-books">
+        //SEARCHBAR&LINK WRAPPER    <div className="search-books-bar">
+        //LINK       <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+        //FORM       <div className="search-books-input-wrapper">
+        //INPUT         <input type="text" placeholder="Search by title or author"/>
+        //       </div>
+        //     </div>
+        //BOOK     <div className="search-books-results">
+        //BOOK       <ol className="books-grid"></ol>
+        //     </div>
+        //   </div>
+        // )
