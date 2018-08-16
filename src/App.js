dd3792983671643cabb7ad401ce1dd78e6ab8d47
Stefanie Chin <chinsk0303@student.laccd.edu>
2018-08-16 13:28:08 -0700

    feat: initialize state

--- src/App.js
+++ src/App.js
@@ -2,26 +2,37 @@ import React, { Component, Fragment } from 'react'
 import { Route } from 'react-router-dom'
 import Main from './Main'
 import SearchPage from './SearchPage'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+
 class BooksApp extends Component {
+  state = {
+        data: []
+      }
+
+  componentDidMount(){
+    BooksAPI.getAll()
+    .then( data => {
+      this.setState({ data });
+    })
+  }
 
   render() {
     return (
       <Fragment>
-      <Route
-      	exact path='/'
-      	render={()=> (
-      		<Main />
-      	)}
-      />
-      <Route
-      	exact path='/SearchPage'
-      	render={()=> (
-	        <SearchPage />
-	    )}
-	  />
+        <Route
+        	exact path='/'
+        	render={()=> (
+        		<Main books={this.state.data}/>
+        	)}
+        />
+        <Route
+        	exact path='/SearchPage'
+        	render={()=> (
+  	        <SearchPage books={this.state.data}/>
+          )}
+        />
       </Fragment>
     )
   }
