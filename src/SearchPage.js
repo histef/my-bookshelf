f45e362ca59828d831c3e9c2f1a3d908d4ee8b2e
Stefanie Chin <chinsk0303@student.laccd.edu>
2018-08-22 21:14:49 -0700

    feat: book search field connected to BooksAPI

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,19 +1,42 @@
 import React, { Component,Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import SearchField from './SearchField'
-// import Book from './Book'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
+
+import escapeRegExp from 'escape-string-regexp'
 
 class SearchPage extends Component {
 
 	state = {
-		query: ''
+		query: '',
+		searchBooks: []
 	}
 
 	updateQuery = (query) => {
 		this.setState({ query })
+		this.showSearched(query);
+	}
+
+
+	showSearched = query => {
+		BooksAPI.search(query)
+			.then((books)=>{
+				this.setState({
+				searchBooks: books
+				})
+			})
 	}
 
 	render(){
+		let matchBooks
+
+		if (this.state.query){
+			const match = new RegExp(escapeRegExp(this.state.query), 'i');
+			matchBooks = this.state.searchBooks.filter( book => match.test(book.title))
+		} else {
+			matchBooks = this.state.searchBooks
+		}
 
 		return (
 
@@ -30,7 +53,15 @@ class SearchPage extends Component {
 					/>
 				</div>
 				<div className='search-books-results'>
-					<ul>
+					<ul className='books-grid'>
+						{matchBooks.map(filteredBook => (
+						<li key={filteredBook.id}>
+							<Book book={filteredBook}/>
+						</li>
+					))
+				}
+
+
 					</ul>
 				</div>
 			</Fragment>
